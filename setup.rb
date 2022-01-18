50e6ec2c8b1178efbf6c7aafdfc7d2c0d646071f
Jenny <j.e.robson@live.com>
2022-01-18 16:38:03 +0000

    updated

--- setup.rb
+++ setup.rb
@@ -106,3 +106,9 @@ CREATE DATABASE "jennmorrison";
 after created just use - psql
 
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
+Connect to psql
+Create the database using the psql command CREATE DATABASE bookmark_manager;
+Connect to the database using the pqsl command \c bookmark_manager;
+# Run the query we have saved in the file 
+  01_create_bookmarks_table.sql
